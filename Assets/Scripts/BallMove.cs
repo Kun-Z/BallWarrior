3f44e0b425240ad5bdb40e18952a8bbce0ce3dc0
Kun-Z <412254545@qq.com>
2018-04-29 21:01:30 +0800

    2018-4-29
    公司

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour {
+    public float VSpeed;
+    public float HSpeed;
+    public Rigidbody rig;
+    Vector3 moveVSpeed;
+    Vector3 moveHSpeed;
+    Vector3 moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        rig = GetComponent<Rigidbody>();
+        moveVSpeed = new Vector3(0, VSpeed,0);
+        moveHSpeed = new Vector3(HSpeed,0,0);
+        rig.velocity = moveVSpeed + moveHSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveSpeed = rig.velocity;
+        transform.position += moveSpeed * Time.deltaTime;
+    }
+
+}
---
+++ Assets/Scripts/ShootControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootControl : MonoBehaviour {
+        public float shootSpeed = 5.0f;
+	    // Use this for initialization
+	    void Start () {
+		
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+		 
+	    }
+}
