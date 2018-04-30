540c710a9b3ffedabd9564aa19cf03da69a72683
Kun-Z <412254545@qq.com>
2018-04-30 16:37:46 +0800

    2018-4-30
    公司

--- Assets/Scripts/BallMove.cs
+++ Assets/Scripts/BallMove.cs
@@ -3,26 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMove : MonoBehaviour {
-    public float VSpeed;
-    public float HSpeed;
-    public Rigidbody rig;
-    Vector3 moveVSpeed;
-    Vector3 moveHSpeed;
-    Vector3 moveSpeed;
-
+    Rigidbody2D rig;
     // Use this for initialization
     void Start () {
-        rig = GetComponent<Rigidbody>();
-        moveVSpeed = new Vector3(0, VSpeed,0);
-        moveHSpeed = new Vector3(HSpeed,0,0);
-        rig.velocity = moveVSpeed + moveHSpeed;
+        rig = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
-	void Update ()
-    {
-        moveSpeed = rig.velocity;
-        transform.position += moveSpeed * Time.deltaTime;
+	void Update () {
+        //print(rig.velocity);
     }
+
 
 }
--- Assets/Scripts/ShootControl.cs
+++ Assets/Scripts/ShootControl.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootControl : MonoBehaviour {
-        public float shootSpeed = 5.0f;
-	    // Use this for initialization
-	    void Start () {
+    public List<GameObject> BallList = new List<GameObject>();
+    // Use this for initialization
+    void Start () {
 		
-	    }
-	
-	    // Update is called once per frame
-	    void Update () {
-		 
-	    }
-}
+	 }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            foreach (GameObject Balls in BallList)
+            {
+                Vector2 mDirection = new Vector2(mTarget.x - Balls.transform.position.x, mTarget.y - Balls.transform.position.y);
+                print(mDirection);
+                Balls.GetComponent<Rigidbody2D>().AddForce(mDirection * 50);
+            }
+        }
+    }
+ }
